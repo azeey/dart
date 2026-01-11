4550bf00b738cb412ba1bd0b0fac9ffec2c267e2
Jeongseok Lee <jslee02@gmail.com>
2026-01-11 14:37:18 -0800

    Fix quoting of CMAKE_CXX_FLAGS for FreeBSD build

--- scripts/freebsd.py
+++ scripts/freebsd.py
@@ -404,6 +404,7 @@ def test_vm(args):
     remote_dir = args.remote_dir or f"/home/{args.user}/dart"
     build_dir = os.getenv("FREEBSD_VM_BUILD_DIR", DEFAULT_BUILD_DIR)
     build_type = os.getenv("FREEBSD_VM_BUILD_TYPE", "Release")
+    eigen_flags = "-DEIGEN_DONT_VECTORIZE=1 -DEIGEN_MAX_ALIGN_BYTES=0"
     cmake_args = [
         f"-DCMAKE_BUILD_TYPE={build_type}",
         "-DDART_BUILD_DARTPY=OFF",
@@ -411,7 +412,7 @@ def test_vm(args):
         "-DDART_ENABLE_SDFORMAT=OFF",
         "-DDART_USE_SYSTEM_GOOGLETEST=ON",
         "-DDART_VERBOSE=ON",
-        "-DCMAKE_CXX_FLAGS=-DEIGEN_DONT_VECTORIZE=1 -DEIGEN_MAX_ALIGN_BYTES=0",
+        f"-DCMAKE_CXX_FLAGS={shlex.quote(eigen_flags)}",
     ]
     cmake_args.extend(shlex.split(os.getenv("FREEBSD_VM_CMAKE_ARGS", "")))
     cmake_arg_str = " ".join(cmake_args)
